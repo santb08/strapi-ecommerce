7b792228f0cc50c04ff8f4930d3ccd7e9392b6cf
Santiago <scb9908@gmail.com>
2023-12-28 12:10:35 -0500

    feat: remove gql

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -3,7 +3,6 @@
  * Any modifications made will be discarded.
  */
 import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
-import graphql from "@strapi/plugin-graphql/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
@@ -11,7 +10,6 @@ import { renderAdmin } from "@strapi/strapi/admin";
 renderAdmin(document.getElementById("strapi"), {
   plugins: {
     "strapi-cloud": strapiCloud,
-    graphql: graphql,
     i18n: i18N,
     "users-permissions": usersPermissions,
   },
